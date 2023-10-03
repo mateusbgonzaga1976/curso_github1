2467d87abca1f30a257d2057483c1405fc9002fe
mateusbgonzaga1976 <mateusbgonzaga@gmail.com>
2023-10-03 18:35:44 -0300

    Nova home finalizada

--- home.js
+++ home.js
@@ -1,3 +1,9 @@
 function mostraNome(nome){
     return nome;
 }
+
+function mostraSobrenome(sobrenome){
+    return sobrenome;
+}
+
+//Finalizado o sistema da home;
--- login.js
+++ login.js
@@ -1,3 +1,12 @@
 //Trabalhando na login
 
 //Finalizando sistema
+
+function login(nome, email){
+    return 'Bem-vindo';
+}
+
+//Seja bem-vindo;
+
+//Sistema de login completo
+//Retornamos o nome do usuario quando logado
